f34731602ce547fabcc7e386ecc4f9576aaa2b75
GrayBurford <GrayBurford@Gmail.com>
2022-11-18 22:28:14 -0500

    second commit -- refactor all promise-based functions to async/await functions

--- deck_of_cards.js
+++ deck_of_cards.js
@@ -5,41 +5,98 @@ const baseURL = 'https://deckofcardsapi.com/api/deck/'
 // TIP: replace <<deck_id>> with "new" to create a shuffled deck and draw cards from that deck in the same request.
 
 // 1)
-axios.get(`${baseURL}new/draw`)
-    .then(resp => {
-        let {suit, value, code, image, images} = resp.data.cards[0]
-        // console.log(resp);
-        // console.log(suit)
-        // console.log(value)
-        console.log(`1))) ${value} OF ${suit}`)
-    })
-    .catch(err => console.log(err))
+// Using Promises
+// axios.get(`${baseURL}new/draw`)
+//     .then(resp => {
+//         let {suit, value, code, image, images} = resp.data.cards[0]
+//         // console.log(resp);
+//         // console.log(suit)
+//         // console.log(value)
+//         console.log(`1))) ${value} OF ${suit}`)
+//     })
+//     .catch(err => console.log(err))
 
+// Using async/await
+async function getCard () {
+    let response = await axios.get(`${baseURL}new/draw`)
+    let {suit, value} = response.data.cards[0]
+    console.log(`1))) ${value} of ${suit}`)
+}
 
 // 2)
 // Must instantiate firstCard here so that we have access to it in the second .then()
+// Using promises
+// let firstCard = '';
+// axios.get(`${baseURL}new/draw`)
+//     .then(resp => {
+//         console.log('2)))', resp);
+//         firstCard = resp.data.cards[0]
+//         console.log('First card:', firstCard)
+//         let deckID = resp.data.deck_id        
+//         return axios.get(`${baseURL}/${deckID}/draw`)
+//     })
+//     .then(resp => {
+//         console.log('2)))', resp);
+//         let secondCard = resp.data.cards[0]
+//         console.log('Second card:', secondCard)
+
+
+
+//         console.log(`First card is: ${firstCard.value} OF ${firstCard.suit} -- Second card is: ${secondCard.value} OF ${secondCard.suit}`)
+//     })
+//     .catch(err => console.log(err))
+
+// Using async/await
 let firstCard = '';
-axios.get(`${baseURL}new/draw`)
-    .then(resp => {
-        console.log('2)))', resp);
-        firstCard = resp.data.cards[0]
-        console.log('First card:', firstCard)
-        let deckID = resp.data.deck_id        
-        return axios.get(`${baseURL}/${deckID}/draw`)
-    })
-    .then(resp => {
-        console.log('2)))', resp);
-        let secondCard = resp.data.cards[0]
-        console.log('Second card:', secondCard)
+async function getTwoCards () {
+    let response = await axios.get(`${baseURL}new/draw`)
+    firstCard = response.data.cards[0]
+    let deckId = response.data.deck_id
 
+    let secondResponse = await axios.get(`${baseURL}/${deckId}/draw`)
+    let secondCard = secondResponse.data.cards[0]
 
+    console.log(`2))) First card is: ${firstCard.value} OF ${firstCard.suit} -- Second card is: ${secondCard.value} OF ${secondCard.suit}`)
 
-        console.log(`First card is: ${firstCard.value} OF ${firstCard.suit} -- Second card is: ${secondCard.value} OF ${secondCard.suit}`)
-    })
-    .catch(err => console.log(err))
+}
 
 
 // 3)
+// Using promises
+// document.addEventListener("DOMContentLoaded", function() {
+
+//     const button = document.getElementById("button")
+//     const title = document.getElementById("title")
+//     const img = document.getElementById("img")
+//     const remaining = document.getElementById("remaining")
+//     const buttonDiv = document.getElementById("button-div")
+//     button.addEventListener('click', drawCard)
+
+//     axios.get(`${baseURL}new/shuffle`)
+//         .then(response => {
+//             console.log('3)))', response.data)
+//             deck_id = response.data.deck_id;
+//             console.log("3))) Deck ID:", deck_id)
+//         })
+
+//     let deck_id = '';
+
+//     function drawCard() {
+//         axios.get(`${baseURL}/${deck_id}/draw`)
+//             .then(response => {
+//                 if (response.data.remaining === 0) {
+//                     buttonDiv.style.display = "none";
+//                 }
+//                 console.log('3)))', response.data);
+//                 title.innerText = `${response.data.cards[0].value} OF ${response.data.cards[0].suit}`;
+//                 img.src = response.data.cards[0].image;
+//                 remaining.innerText = response.data.remaining;
+//             })
+//             .catch(err => console.log(err))
+//     }
+// });
+
+// Using async/await
 document.addEventListener("DOMContentLoaded", function() {
 
     const button = document.getElementById("button")
@@ -49,50 +106,57 @@ document.addEventListener("DOMContentLoaded", function() {
     const buttonDiv = document.getElementById("button-div")
     button.addEventListener('click', drawCard)
 
-    axios.get(`${baseURL}new/shuffle`)
-        .then(response => {
-            console.log('3)))', response.data)
-            deck_id = response.data.deck_id;
-            console.log("3))) Deck ID:", deck_id)
-        })
+    let deckId = '';
 
-    let deck_id = '';
-
-    function drawCard() {
-        axios.get(`${baseURL}/${deck_id}/draw`)
-            .then(response => {
-                if (response.data.remaining === 0) {
-                    buttonDiv.style.display = "none";
-                }
-                console.log('3)))', response.data);
-                title.innerText = `${response.data.cards[0].value} OF ${response.data.cards[0].suit}`;
-                img.src = response.data.cards[0].image;
-                remaining.innerText = response.data.remaining;
-            })
-            .catch(err => console.log(err))
+    async function buildNewDeck () {
+        let resp = await axios.get(`${baseURL}new/shuffle`)
+        deckId = resp.data.deck_id
+        console.log("3))) ASYNC DECK ID:", deckId)
     }
 
+    buildNewDeck()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    async function drawCard () {
+        let resp = await axios.get(`${baseURL}/${deckId}/draw`)
+        if (resp.data.remaining === 0) {
+            buttonDiv.style.display = "none";
+        }
+        title.innerText = `${resp.data.cards[0].value} OF ${resp.data.cards[0].suit}`;
+        img.src = resp.data.cards[0].image;
+        remaining.innerText = resp.data.remaining;
+    }
 
 });
 
+
+// EXAMPLE OBJECT WITH ASYNC FUNCTIONS:
+const deck = {
+    async init() {
+        let res = await axios.get('https://deckofcardsapi.com/api/deck/new/')
+        this.deckId = res.data.deck_id;
+    },
+    async shuffleCards () {
+        let res = await axios.get(`https://deckofcardsapi.com/api/deck/${this.deckId}/shuffle`)
+        console.log(res)
+    },
+    async drawCard () {
+        axios.get(`https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=1`)
+        console.log(res.data)
+    }
+}
+
+// EXAMPLE CLASS WITH ASYNC FUNCTION:
+class Pokemon {
+    constructor(id) {
+        this.id = id;
+        this.types = [];
+    }
+    
+    async getInfo() {
+        let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${this.id}`)
+        this.name = res.data.name;
+        for (let type of res.data.types) {
+        this.types.push(type.type.name)
+        }
+    }
+    }
--- number_facts.js
+++ number_facts.js
@@ -11,46 +11,91 @@ const ul3 = document.getElementById('ul-num-3')
 
 
 // 1)
-axios.get(`${BASEURL}/${myNum}/${type}?json`)
-    .then(response => {
-        console.log('1)))', response);
-        let li = document.createElement("li");
-        li.innerText = response.data.text
-        ul1.append(li);
-    })
+// Using promises
+// axios.get(`${BASEURL}/${myNum}/${type}?json`)
+//     .then(response => {
+//         console.log('1)))', response);
+//         let li = document.createElement("li");
+//         li.innerText = response.data.text
+//         ul1.append(li);
+//     })
 
+// Using async/await
+async function getNum () {
+    let res = await axios.get(`${BASEURL}/${myNum}/${type}?json`)
+    console.log(res)
+    let li = document.createElement("li");
+    li.innerText = res.data.text
+    ul1.append(li);
+}
 
 // 2)
-axios.get(`${BASEURL}/${[100, 250, 500, 750]}/${type}?json`)
-    .then(response => {
-        console.log('2)))', response);
-        let respObj = response.data;
+// Using Promises
+// axios.get(`${BASEURL}/${[100, 250, 500, 750]}/${type}?json`)
+//     .then(response => {
+//         console.log('2)))', response);
+//         let respObj = response.data;
+//         for (let each of Object.values(respObj)) {
+//             let li = document.createElement("li");
+//             li.innerText = each;
+//             ul2.append(li);
+//         }
+//     })
+
+// Using async/await
+async function getMultipleNums () {
+    let res = await axios.get(`${BASEURL}/${[100, 250, 500, 750]}/${type}?json`)
+    console.log(res)
+    let respObj = res.data;
         for (let each of Object.values(respObj)) {
             let li = document.createElement("li");
             li.innerText = each;
             ul2.append(li);
         }
-    })
-
+}
 
 // 3)
+// Using Promises
+
+// let myFavNum = 3;
+// let allPromises = [];
+
+// for (let i = 1; i < 5; i++) {
+//     allPromises.push(
+//         axios.get(`${BASEURL}/${myFavNum}/${type}`)
+//     );
+// };
+
+// Promise.all(allPromises, console.log(allPromises))
+//     .then(array => {
+//         console.log('3)))', array);
+//         for (let promise of array) {
+//             console.log(promise.data)
+//             let li = document.createElement("li");
+//             li.innerText = promise.data;
+//             ul3.append(li);
+//         }
+//     })
+//     .catch(error => console.log(error))
+
+// Using async/await
 let myFavNum = 3;
-let allPromises = [];
 
-for (let i = 1; i < 5; i++) {
-    allPromises.push(
+async function getAllPromises () {
+    await Promise.all([
+        axios.get(`${BASEURL}/${myFavNum}/${type}`),
+        axios.get(`${BASEURL}/${myFavNum}/${type}`),
+        axios.get(`${BASEURL}/${myFavNum}/${type}`),
         axios.get(`${BASEURL}/${myFavNum}/${type}`)
-    );
-};
+    ])
+    .then(response => {
+        console.log(response)
 
-Promise.all(allPromises, console.log(allPromises))
-    .then(array => {
-        console.log('3)))', array);
-        for (let promise of array) {
-            console.log(promise.data)
+        for (let promise of response) {
             let li = document.createElement("li");
             li.innerText = promise.data;
             ul3.append(li);
         }
     })
-    .catch(error => console.log(error))
+    
+}
